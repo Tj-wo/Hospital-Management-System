d6c9ee8184b7f86d7c15958e7cfdae74ee0c661c
Tj-wo <jovanssemivule@gmail.com>
2025-06-30 11:47:49 +0300

    Updated beans to handle new views

--- src/main/java/org/pahappa/controller/LoginFilter.java
+++ src/main/java/org/pahappa/controller/LoginFilter.java
@@ -1,47 +1,41 @@
-package org.pahappa.controller;
+package org.pahappa.filters;
 
-import javax.inject.Inject;
-import javax.servlet.Filter;
-import javax.servlet.FilterChain;
-import javax.servlet.FilterConfig;
-import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
-///@WebFilter("*.xhtml") // Intercept every request for an .xhtml page
+@WebFilter("*.xhtml")
 public class LoginFilter implements Filter {
 
-    @Inject
-    private LoginBean loginBean;
-
     @Override
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-        HttpServletRequest req = (HttpServletRequest) request;
-        HttpServletResponse res = (HttpServletResponse) response;
-        String reqURI = req.getRequestURI();
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
 
-        // Check if the user is logged in
-        boolean loggedIn = (loginBean != null && loginBean.isLoggedIn());
+        try {
+            HttpServletRequest req = (HttpServletRequest) request;
+            HttpServletResponse res = (HttpServletResponse) response;
+            HttpSession session = req.getSession(false);
 
-        // Define public pages that don't require login
-        boolean loginRequest = reqURI.endsWith("/login.xhtml");
-        boolean registrationRequest = reqURI.endsWith("/register.xhtml"); // <-- NEW
-        boolean resourceRequest = reqURI.startsWith(req.getContextPath() + "/javax.faces.resource");
+            String reqURI = req.getRequestURI();
+            boolean loggedIn = session != null && session.getAttribute("user") != null;
+            boolean loginRequest = reqURI.endsWith("/login.xhtml");
+            boolean resourceRequest = reqURI.contains("javax.faces.resource");
 
-        // If user is logged in OR is accessing a public page, let them through
-        if (loggedIn || loginRequest || registrationRequest || resourceRequest) {
-            chain.doFilter(request, response);
-        } else {
-            // User is not logged in and is trying to access a protected page. Redirect to login.
-            res.sendRedirect(req.getContextPath() + "/login.xhtml");
+            if (loggedIn || loginRequest || resourceRequest) {
+                chain.doFilter(request, response);
+            } else {
+                res.sendRedirect(req.getContextPath() + "/login.xhtml");
+            }
+        } catch (Exception e) {
+            System.out.println("LoginFilter error: " + e.getMessage());
         }
     }
 
     @Override
-    public void init(FilterConfig filterConfig) throws ServletException {}
+    public void init(FilterConfig config) throws ServletException {}
 
     @Override
     public void destroy() {}
